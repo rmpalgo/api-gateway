2aecff14c28f7d396dd6d7f6f83d2aaf6f0587d8
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-31 21:05:11 -0500

    feat: integrated country api as a route in gateway.

--- src/main/java/com/apigateway/apigateway/GatewayConfiguration.java
+++ src/main/java/com/apigateway/apigateway/GatewayConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static org.springframework.boot.actuate.metrics.web.client.RestTemplateExchangeTags.uri;
+
 @EnableHystrix
 @Configuration
 public class GatewayConfiguration {
@@ -13,6 +15,13 @@ public class GatewayConfiguration {
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
+
+                .route(p -> p
+                            .path("/all")
+                            .filters(f -> f.addRequestHeader("x-rapidapi-host", "restcountries-v1.p.rapidapi.com")
+                                            .addRequestHeader(	"x-rapidapi-key", "00ef74d087mshaefe9522e030026p159d49jsn5af7baf07142"))
+                            .uri("https://restcountries-v1.p.rapidapi.com")
+        )
                 .route(p -> p
                         .path("/get")
                         .filters(f -> f.addRequestHeader("Hello", "World"))
